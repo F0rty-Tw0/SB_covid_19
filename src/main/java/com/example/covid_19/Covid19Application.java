94d29e8ea3bf37a2616fdbf8a736b77d2606e781
Artiom Tofan <artiomtofancv@gmail.com>
2021-05-04 15:39:37 +0200

    added dev brachn

--- src/main/java/com/example/covid_19/Covid19Application.java
+++ src/main/java/com/example/covid_19/Covid19Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Covid19Application {
-
+//DEV 
 	public static void main(String[] args) {
 		SpringApplication.run(Covid19Application.class, args);
 	}
