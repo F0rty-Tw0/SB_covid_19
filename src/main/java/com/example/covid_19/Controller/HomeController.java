7bf1a428f3e092ee7ab5a95205185e3f5c7bf6fa
pawe0773 <pawe0773@stud.kea.dk>
2021-05-12 18:14:03 +0200

    Added about page and three bottom columns with responsivnes.

--- src/main/java/com/example/covid_19/Controller/HomeController.java
+++ src/main/java/com/example/covid_19/Controller/HomeController.java
@@ -1,12 +1,18 @@
 package com.example.covid_19.Controller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@Controller
 public class HomeController {
     @GetMapping("/")
     public String index(){
         return "index";
 
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about/about";
     }
 }
