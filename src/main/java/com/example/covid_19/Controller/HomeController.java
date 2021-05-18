286aacd3f7735d57769e4b6ede5f0096a4156750
Artiom Tofan <artiomtofancv@gmail.com>
2021-05-18 15:37:02 +0200

    made the models

--- src/main/java/com/example/covid_19/Controller/HomeController.java
+++ src/main/java/com/example/covid_19/Controller/HomeController.java
@@ -1,33 +1,33 @@
 package com.example.covid_19.Controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String index(){
+    public String index() {
         return "index";
 
     }
 
     @GetMapping("/about")
-    public String about(){
+    public String about() {
         return "about";
     }
 
     @GetMapping("/test")
-    public String test(){
+    public String test() {
         return "test";
     }
 
     @GetMapping("/vaccine")
-    public String vaccine(){
+    public String vaccine() {
         return "vaccine";
     }
 
     @GetMapping("/user")
-    public String user(){
+    public String user() {
         return "user";
     }
 }
