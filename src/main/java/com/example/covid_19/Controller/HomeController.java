d88b3f26ed02955f2d5455120c07e21e09333de4
Artiom Tofan <artiomtofancv@gmail.com>
2021-05-20 19:57:30 +0200

    password validation

--- src/main/java/com/example/covid_19/Controller/HomeController.java
+++ src/main/java/com/example/covid_19/Controller/HomeController.java
@@ -7,6 +7,8 @@ import com.example.covid_19.Model.Booking;
 import com.example.covid_19.Model.User;
 import com.example.covid_19.Service.UserService.UserServiceInterface;
 import com.example.covid_19.Service.BookingService.BookingServiceInterface;
+import com.example.covid_19.Service.PasswordService.PasswordServiceInterface;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
@@ -16,31 +18,34 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
     @Autowired
     private UserServiceInterface users;
-    User user = new User(83, "Nikolai Tofan", "string", "art@gmail.com", 1234565, 9999999, "TESAT WAY", 1, 1) {
-    };
-
-    String str = "2015-03-31";
-    Date date = Date.valueOf(str);// converting string into sql date
 
     @Autowired
     private BookingServiceInterface bookings;
+
+    @Autowired
+    private PasswordServiceInterface password;
+
+    String str = "2015-03-31";
+    Date date = Date.valueOf(str);// converting string into sql date
 
     Booking booking = new Booking(2, "Nikolai", 2, date);
 
     @GetMapping("/")
     public String index() {
-        String password = "plainPassword";
-        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder(10, new SecureRandom());
-        String encodedPassword = bCryptPasswordEncoder.encode(password);
-        System.out.println(encodedPassword);
-        System.out.println(bCryptPasswordEncoder.matches(password, encodedPassword));
+        // User user = new User(83, "Nikolai Tofan", password.encrypt("string"),
+        // "sart@gmail.com", 1934565, 9999999, "TESAT WAY", 1, 1) {
+        // };
 
-        try {
-            users.addUser(user);
-        } catch (RuntimeException e) {
-            System.out.println(e.getMessage());
-        }
-        
+        // try {
+        // users.addUser(user);
+        // } catch (RuntimeException e) {
+        // System.out.println(e.getMessage());
+        // }
+
+        User user = users.findUserById(111);
+
+        System.out.println(password.match("string", user.getUserPassword()));
+
         return "index";
     }
 
