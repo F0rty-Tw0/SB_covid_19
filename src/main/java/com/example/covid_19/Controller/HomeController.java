3a398458941d94b9d0a94458754cdcf071445a49
Artiom Tofan <artiomtofancv@gmail.com>
2021-05-31 10:37:47 +0200

    moved file creation on project start

--- src/main/java/com/example/covid_19/Controller/HomeController.java
+++ src/main/java/com/example/covid_19/Controller/HomeController.java
@@ -3,8 +3,6 @@ package com.example.covid_19.Controller;
 import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import java.io.File; // Import the File class
-import java.io.IOException; // Import the IOException class to handle errors
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,20 +11,6 @@ public class HomeController {
     @GetMapping("/")
     public String index() {
         // minimumRequirements();
-        // This code exists here only because it was asked in assignment.
-        // We wouldn't store our logs inside a file otherwise.
-        try {
-            File myObj = new File("errorLog.txt");
-
-            if (myObj.createNewFile()) {
-                System.out.println("File created: " + myObj.getName());
-            } else {
-                System.out.println("File already exists.");
-            }
-        } catch (IOException e) {
-            System.out.println("An error occurred.");
-            e.printStackTrace();
-        }
         return "index";
     }
 
--- src/main/java/com/example/covid_19/Covid19Application.java
+++ src/main/java/com/example/covid_19/Covid19Application.java
@@ -1,5 +1,7 @@
 package com.example.covid_19;
 
+import java.io.File;
+import java.io.IOException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +9,22 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Covid19Application {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		SpringApplication.run(Covid19Application.class, args);
+
+		// This code exists here only because it was asked in assignment.
+		// We wouldn't store our logs inside a file otherwise.
+		try {
+			File myObj = new File("errorLog.txt");
+
+			if (myObj.createNewFile()) {
+				System.out.println("File created: " + myObj.getName());
+			} else {
+				System.out.println("File already exists.");
+			}
+		} catch (IOException e) {
+			System.out.println("An error occurred.");
+			e.printStackTrace();
+		}
 	}
 }
